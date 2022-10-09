e0f2c3b7b500a5654512ada9d78d24e4f83f8ab3
prasanna-AR <115212325+prasanna-AR@users.noreply.github.com>
2022-10-09 10:11:17 -0200

    Add files via upload

---
+++ mid to last led blink using binary number.c
@@ -0,0 +1,61 @@
+/****************/
+/* Author : Prasanna */
+/* Date   : 29-09-2022 */
+/* Filename:middle to last using binary number led blink*/
+/* Description :middle to last  */
+/****************/
+#include<htc.h>
+#include<stdio.h>
+#define _XTAL_FREQ 20000000
+__CONFIG(FOSC_HS & WDTE_OFF & PWRTE_OFF & BOREN_ON & LVP_OFF);
+
+void main()
+{
+	TRISB=0;//port bas output
+	while(1)
+	{
+		
+		PORTB=0B00000000;
+		__delay_ms(300);
+		PORTB=0B00000000;
+		__delay_ms(500);
+		
+		PORTB=0B00000000;
+		__delay_ms(300);
+		PORTB=0B00000000;
+		__delay_ms(500);
+		
+		PORTB=0B00000000;
+		__delay_ms(300);
+		PORTB=0B00000000;
+		__delay_ms(500);
+		
+		PORTB=0B00000000;
+		__delay_ms(300);
+		PORTB=0B00000000;
+		__delay_ms(500);
+		
+		PORTB=0B00010000;
+		__delay_ms(300);
+		PORTB=0B00000000;
+		__delay_ms(500);
+		
+		PORTB=0B00100000;
+		__delay_ms(300);
+		PORTB=0B00000000;
+		__delay_ms(500);
+		
+		PORTB=0B01000000;
+		__delay_ms(300);
+		PORTB=0B00000000;
+		__delay_ms(500);
+		
+		PORTB=0B10000000;
+		__delay_ms(300);
+		PORTB=0B00000000;
+		__delay_ms(500);
+		
+	}
+	
+}			
+	
